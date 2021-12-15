2f5871d9991f92082ddd96fb046653f7092e6f59
Isabella Bertini <isabella@kenzie.com.br>
2021-12-15 13:48:37 -0300

    Iniciando com js

---
+++ inicio.js
@@ -0,0 +1,75 @@
+/*
+aritmetica basica => adição, subtração, multiplicação, divisão
+
+variaveis 
+
+tipos de variaveis
+condicional = IF/ELSE
+comparadores
+funções
+repetição (looping) = FOR
+estruturas de informações (arrays)
+*/
+
+/*------------------------------------------------------------*/
+
+/*
+
++
+-
+* = multiplicação -> 2*3 = 2+2+2
+
+/
+^
+
+% => divisão com resto -> 2/2 = resto 0
+
+*/
+
+const numero = 2; //number/float
+let letras = 'abc'; //string 'abc' === 'Abc'
+
+letras = 'svg';
+
+/* let
+const */
+
+function escopo(){
+
+    if(){
+        //
+        //
+        //
+        //
+    }
+
+    for(){
+
+    }
+
+    if(){
+ //
+ //
+ //
+ //
+
+    }
+}
+/*
+
+= : atribuir valor
+
+== : comparo um valor '2' == 2
+=== : identico 2 === 2
+
+*/
+
+/*
+
+string => textos '' / ""
+numero => numeros
+booleano => true/false | verdadeiro/falso
+
+*/
+
+letras = 'xkv';
